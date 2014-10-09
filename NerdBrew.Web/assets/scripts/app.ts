20f61c042a5e0a93716e39d911c2f83589feac6f
Joshua Beardsley <jbeardsl@nerdery.com>
2014-10-09 11:44:34 -0500

    Initial commit

---
+++ NerdBrew.Web/assets/scripts/app.ts
@@ -0,0 +1,5 @@
+﻿/// <reference path="definitions.ts"/>
+/// <reference path="controllers/brewListController.ts"/>
+
+var app = angular.module('NerdBrewApp', []);
+app.controller('brewListController', BrewListController);
